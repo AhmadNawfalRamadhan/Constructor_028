5e4d6de61bc554356e0434d82d406c3445c2f8da
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-05-17 15:49:14 +0700

    mbrStatikfunc22

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -17,3 +17,11 @@ public:
 
     mahasiswa(string pnama):nama(pnama){setID();}
 };
+
+int mahasiswa::nim=0;
+
+void mahasiswa::printAll(){
+    cout<<"ID   = " <<id<<endl;
+    cout<<"Nama = " <<nama<<endl;
+    cout<<endl;
+}
